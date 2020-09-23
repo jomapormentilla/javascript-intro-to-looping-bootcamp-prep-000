b2ff4fc566a3bc6910b733705cbf529811a3914b
Joma Pormentilla <jomapormentilla@gmail.com>
2020-09-23 20:11:12 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -15,12 +15,15 @@ function whileLoop(n){
 }
 
 function doWhileLoop(num){
+  let i = 0
+  
   function incrementVariable() {
     i = i + 1;
     return i;
   }
   
   do{
+    console.log("I run once regardless.")
     incrementVariable()
   }while(incrementVariable() < num)
 }
