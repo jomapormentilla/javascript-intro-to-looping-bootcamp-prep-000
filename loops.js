f5e3f65e460d20a22639e0bdff5e97276c858c42
Joma Pormentilla <jomapormentilla@gmail.com>
2020-09-23 19:52:37 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array){
-  for(let i=0; i<25; i++){
-    array[i] = `I am ${i} strange loop${ i === 0 ? '' : 's' }.`
+  for(let i=1; i<=25; i++){
+    array[i] = `I am ${i} strange loop${ i === 1 ? '' : 's' }.`
   }
   return array
 }
